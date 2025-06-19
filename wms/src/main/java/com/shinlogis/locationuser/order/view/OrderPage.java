7301690a170e7382c764811f0f1f8abc39c0a104
dev-Kiwi7 <seodalmi.u@gmail.com>
2025-06-19 20:13:56 +0900

    0619 지점페이지 연결

---
+++ wms/src/main/java/com/shinlogis/locationuser/order/view/OrderPage.java
@@ -0,0 +1,15 @@
+package com.shinlogis.locationuser.order.view;
+
+import java.awt.Color;
+
+import com.shinlogis.wms.AppMain;
+import com.shinlogis.wms.common.config.Page;
+
+public class OrderPage extends Page{
+
+	public OrderPage(AppMain appMain) {
+		super(appMain);
+		
+		setBackground(Color.PINK);
+	}
+}
