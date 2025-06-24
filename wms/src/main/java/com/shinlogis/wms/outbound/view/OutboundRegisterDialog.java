4b2daccb5e623fd7c37fec569d741f242ff9dad5
unknown <henrys200632@gmail.com>
2025-06-24 11:47:20 +0900

    sql 수정

--- wms/src/main/java/com/shinlogis/wms/outbound/view/OutboundRegisterDialog.java
+++ wms/src/main/java/com/shinlogis/wms/outbound/view/OutboundRegisterDialog.java
@@ -1,23 +1,32 @@
 package com.shinlogis.wms.outbound.view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class OutboundRegisterDialog extends JDialog{
-	JPanel p_title;
+public class OutboundRegisterDialog extends JDialog {
+
+	JPanel p_table;
+
 	public OutboundRegisterDialog() {
-		p_title = new JPanel();
-		p_title.setPreferredSize(new Dimension(700,800));
-		p_title.setBackground(Color.PINK);
+		// 제목 서식.
+		setTitle("출고 예정 등록");
 		
-		
-		System.out.println("등록다이얼로그가 생성 되었습니다.");
-		add(p_title);
-		setSize(new Dimension(700,800));
+		//본문서식
+		p_table = new JPanel();
+		p_table.setPreferredSize(new Dimension(700,700));
+		p_table.setBackground(Color.CYAN);
+
+		add(p_table, BorderLayout.SOUTH);
+		setBounds(500, 200, 0, 0);
+		setSize(new Dimension(700, 800));
 		setVisible(true);
 	}
-	
+
 }
